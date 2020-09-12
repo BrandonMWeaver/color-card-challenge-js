d833b03fa76b75c1482e61e5cb8d91139321b456
Brandon Weaver <a7xncob@gmail.com>
2020-09-11 20:42:23 -0400

    Add card component and cards container.

---
+++ src/containers/CardsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Card from '../components/Card';
+
+const CardsContainer = props => {
+    return (
+        <div className="CardsContainer">
+        {props.colors.map((color, index) => {
+            <Card key={index} color={color} />
+        })}
+        </div>
+    );
+}
+
+export default CardsContainer;
